a7c2a931a554221327ff3d21a7252acb13b1cb4d
vinaykumar164 <venkatavinay43@gmail.com>
2026-06-07 21:28:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,25 +4,35 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-   return """
+    return """
 <!DOCTYPE html>
 <html>
 <head>
     <title>Production App</title>
     <style>
         body {
-            background:#0f172a;
-            color:white;
-            text-align:center;
-            padding-top:100px;
-            font-family:Arial;
+            background: #0f172a;
+            color: white;
+            text-align: center;
+            padding-top: 100px;
+            font-family: Arial, sans-serif;
         }
+
         .card {
-            background:#1e293b;
-            width:500px;
-            margin:auto;
-            padding:40px;
-            border-radius:10px;
+            background: #1e293b;
+            width: 500px;
+            margin: auto;
+            padding: 40px;
+            border-radius: 10px;
+            box-shadow: 0px 0px 20px rgba(255,255,255,0.1);
+        }
+
+        h1 {
+            color: #38bdf8;
+        }
+
+        p {
+            font-size: 18px;
         }
     </style>
 </head>
@@ -30,7 +40,9 @@ def home():
     <div class="card">
         <h1>ECS Fargate Deployment Successful</h1>
         <p>Application accessible via ALB</p>
-        <p>EventBridge scheduler active</p>
+        <p>Jenkins CI/CD Pipeline Working</p>
+        <p>ECR Image Deployment Successful</p>
+        <p>AWS Region: ap-south-2</p>
     </div>
 </body>
 </html>
@@ -41,4 +53,4 @@ def health():
     return "OK", 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=80)
